9986feec8e849550420f2f75500e522a22050f1f
rodricksnyc <rodricksnyc@gmail.com>
2018-07-01 21:51:49 -0400

    files

--- public/javascripts/canvas.js
+++ public/javascripts/canvas.js
@@ -3,6 +3,21 @@ document.addEventListener("DOMContentLoaded", function() {
 
 context = document.querySelector('#drawingCanvas').getContext("2d");
 
+//define the canvas
+let canvas = document.querySelector('#   ');
+
+//global mousdown variable for when the mouse is pressed
+let mouseDown = false;
+
+canvas.addEventListener('mousedown', function(event) {
+  mouseDown = true;
+  console.log(mouseDown);
+})
+canvas.addEventListener('mouseup', function(event) {
+  mouseDown = false;
+  console.log(mouseDown);
+})
+
 
 
 
