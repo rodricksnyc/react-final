b04e64b23b0889a93db42f678cdbe076ba401c5e
rodricksnyc <rodricksnyc@gmail.com>
2018-07-28 09:22:51 -0400

    efwwefwe

--- app.js
+++ app.js
@@ -35,11 +35,11 @@ app.use('/drawings', drawingsRouter);
 
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/src'));
+	app.use(express.static('client/build'));
 }
 
 app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/src', 'index.html'));
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 // app.use(function(req, res, next) {
