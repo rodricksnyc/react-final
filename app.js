6740d032eadf60425bb1a0eb13f674ec7d7ab7a4
rodricksnyc <rodricksnyc@gmail.com>
2018-07-27 21:36:07 -0400

    chwwf

--- app.js
+++ app.js
@@ -35,11 +35,11 @@ app.use('/drawings', drawingsRouter);
 
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/public'));
+	app.use(express.static(path.join(__dirname, 'client/build')));
 }
 
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client', 'public', 'index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 // app.use(function(req, res, next) {
